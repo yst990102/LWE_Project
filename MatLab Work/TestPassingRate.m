5f5747ab8476638b42a6c5f3d9429f7d75e9d65c
Idriz-hax <idriz.hax@gmail.com>
2021-10-05 20:16:54 +1100

    Time measuring begun adding
    Added tic and toc to measure encrypt and decrypt, arrays to hold time taken for each aspect (encrypt and decrypt) and begun output lines

--- MatLab Work/TestPassingRate.m
+++ MatLab Work/TestPassingRate.m
@@ -3,7 +3,11 @@ test_nums = 1000;
 for config_num = 0:3
     fprintf("-------Testing configuration %d------- \n", config_num);
     success_count = 0;
+    time_encrypt = zeros(1,test_nums);
+    time_decrypt = zeros(1,test_nums);
+    
     for test_num = 1:test_nums
+        tic
         [q,A,e,s] = generator(config_num);
 
         [A_row, A_col] = size(A);
@@ -13,6 +17,8 @@ for config_num = 0:3
 
         binary_string = StringToBinary("AbcD", 8);
         uv_cells = EncryptCharToUV(binary_string,B,A,q);
+        time_encrypt(test_num) = toc;
+        tic
 
         [char_num, char_length] = size(binary_string);
         
@@ -22,9 +28,11 @@ for config_num = 0:3
 
             DecryptResult(j,:) = DecryptUVToChar(uv_cell,q,s);
         end
-
+        
+        time_decrypt(test_num) = toc;
         success_count = success_count + CheckInputOutputMatch(binary_string, DecryptResult);
     end
 
     fprintf("Total test cases: %d, success cases: %d, success rate: %.2f%%. \n",test_nums, success_count, (success_count / test_nums) * 100 );
+    fprintf("Total time taken: %.2fs \n", (sum(time_encrypt) + sum(time_decrypt)));
 end
