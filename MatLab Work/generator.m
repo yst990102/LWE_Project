badcb34231361f536b2ac6873fc2217b965fd577
Idriz-hax <idriz.hax@gmail.com>
2021-10-30 09:12:07 +1100

    Removed e generator removed

--- MatLab Work/generator.m
+++ MatLab Work/generator.m
@@ -10,8 +10,8 @@ if configuration_num == 0
     q_max = 79;
     
     % setting for e
-    e_min = -2;
-    e_max = 2;
+    %e_min = -2;
+    %e_max = 2;
 
 elseif configuration_num == 1
     % setting for A
@@ -23,8 +23,8 @@ elseif configuration_num == 1
     q_max = 128;
     
     % setting for e
-    e_min = -1;
-    e_max = 1;
+    %e_min = -1;
+    %e_max = 1;
 
 elseif configuration_num == 2
     % setting for A
@@ -36,8 +36,8 @@ elseif configuration_num == 2
     q_max = 8192;
     
     % setting for e
-    e_min = -4;
-    e_max = 4;
+    %e_min = -4;
+    %e_max = 4;
 
 elseif configuration_num == 3
     % setting for A
@@ -49,8 +49,8 @@ elseif configuration_num == 3
     q_max = 65535;
     
     % setting for e
-    e_min = -16;
-    e_max = 16;
+    %e_min = -16;
+    %e_max = 16;
 
 else
     error('configuration_num need to be [1,2,3]');
@@ -63,7 +63,7 @@ q = q_r(randperm(numel(q_r),1));
 
 A = randi([0,q],A_row,A_col);
 % e = randi([e_min,e_max],A_row,1);
-e = round(normrnd(0,0.341,A_row,1));
+%e = round(normrnd(0,0.341,A_row,1));
 s = randi([0, q],A_col,1);
 
 return
