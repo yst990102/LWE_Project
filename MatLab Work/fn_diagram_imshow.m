497727b3e82f1907ba91d393f9827b0f55bd4aa1
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2021-11-01 23:53:53 +0800

    2021-11-01
    (MATLAB) add generateA/S/E_vhdl file, for generate formatted matrices provided in vhdl coding.
    (MATLAB) add GetFirstSignificantBit.m
    (MATLAB) fn_diagram_imshow.m now can generate for all types appro_multipliers with customised test_nums , sz, k

--- MatLab Work/fn_diagram_imshow.m
+++ MatLab Work/fn_diagram_imshow.m
@@ -1,9 +1,32 @@
 clc;
 clear;
-M = ones(1000, 1000);
-for i = 1:1000
-    for j = 1:1000
-        M(i,j) = (i*j - fn_MitchellMul_MBM_t(i,j,16,15));
+
+fn_multiplier = input("Enter approximate-multi choice (1 - MBM, 2 - Optimized MBM, 3 - REALM8x8):", 's');
+fn_multiplier_choice = str2double(fn_multiplier);
+
+test_nums_input = input("Enter test cases:", 's');
+test_nums = str2double(test_nums_input);
+
+sz_str = input("Enter sz: ", 's');
+sz = str2double(sz_str);
+
+if ~fn_multiplier == 2
+    k_str = input("Enter k: ", 's');
+    k = str2double(k_str);
+end
+
+M = zeros(test_nums, test_nums);
+for i = 1:test_nums
+    for j = 1:test_nums
+        % M(i,j) = (i*j - (fn_MitchellMul_MBM_t(i,j,16,16) - 0.08333*2^(GetFirstSignificantBit(dec2bin(i,8)) + GetFirstSignificantBit(dec2bin(i,8)))) );
+        if fn_multiplier_choice == 1
+            M(i,j) = (i*j - fn_MitchellMul_MBM_t(i,j,sz,k));
+        elseif fn_multiplier_choice == 2
+            M(i,j) = (i*j - fn_MitchellMul_Optimized(i,j,sz));
+        else
+            M(i,j) = (i*j - fn_MitchellMul_REALM8x8_t(i,j,sz,sz));
+        end
+        
     end
 end
 M = M / max(M(:));
